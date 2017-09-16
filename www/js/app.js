e2b75c7dda624fb6d58d630fbe965835b01dd566
Adrian Barwicki <adrianbarwicki@gmail.com>
2017-09-16 15:56:28 +0200

    first showcase of a mvp

--- www/js/app.js
+++ www/js/app.js
@@ -1,5 +1,20 @@
 angular.module('starter', ['ionic', 'starter.controllers', 'starter.directives'])
 
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+      .state('driving', {
+          templateUrl: 'templates/driving.html',
+          url: '/driving',
+          controller: 'AppCtrl'
+      })
+      .state('results', {
+          url: '/results',
+          templateUrl: 'templates/results.html',
+          controller: 'ResultCtrl'
+      })
+  $urlRouterProvider.otherwise('/driving');
+})
+
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     if(window.StatusBar) {
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,27 +1,104 @@
 angular.module('starter.controllers', [])
 
-.controller('MapCtrl', function($scope, $ionicLoading) {
-  $scope.mapCreated = function(map) {
-    $scope.map = map;
-  };
+.service('hzTripRewards', function() {
+  var rewards = [];
 
-  $scope.centerOnMe = function () {
-    console.log("Centering");
-    if (!$scope.map) {
-      return;
+  return {
+    addReward: function(totalDistance, withFocus, withoutFocus) {
+      // the use of the app is rewarded, driving with focus is incentivied while witout is being penalised
+      var points = totalDistance + (2 * withFocus) - (10 * withoutFocus);
+
+      // we do not allow negative points... negative feedback to the user!
+      points = points < 0 ? 0 : points.toFixed(0);
+
+      rewards.push({
+        points: points,
+        totalDistance: totalDistance.toFixed(1),
+        withFocus: withFocus.toFixed(1),
+        withoutFocus: withoutFocus.toFixed(1)
+      })
+    },
+    getLast: function() {
+      return rewards[rewards.length - 1]
     }
-
-    $scope.loading = $ionicLoading.show({
-      content: 'Getting current location...',
-      showBackdrop: false
-    });
-
-    navigator.geolocation.getCurrentPosition(function (pos) {
-      console.log('Got pos', pos);
-      $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
-      $scope.loading.hide();
-    }, function (error) {
-      alert('Unable to get location: ' + error.message);
-    });
   };
+})
+
+.controller('AppCtrl', function($scope, $state, $interval, hzTripRewards) {
+  $scope.drivingData = null;
+  $scope.isDriveMode = false;
+  $scope.isFocused = true;
+  $scope.start = start;
+  $scope.stop = stop;
+
+  var runningInterval;
+
+  initDrivingData();
+
+  // private
+  function initDrivingData() {
+    $scope.drivingData = {
+      lastFocusDistance: 0,
+      totalDistance: 0,
+      withFocus: 0,
+      withoutFocus: 0
+    };
+  }
+
+  // public
+  function start() {
+    $scope.isDriveMode = true;
+
+    runningInterval = $interval(function() {
+      var distanceDelta = 0.2;
+      
+      /**
+       * HARD-CODED ALERT WHEN USER LOOSES FOCUS ON THE ROAD
+       */
+      if ($scope.drivingData.totalDistance > 1.0 && $scope.drivingData.totalDistance < 2.4) {
+        $scope.isFocused = false;
+
+        $scope.drivingData.lastFocusDistance = 0;
+        $scope.drivingData.withoutFocus = Math.round(($scope.drivingData.withoutFocus + distanceDelta) * 100) / 100;
+      } else {
+        $scope.isFocused = true;
+
+        $scope.drivingData.lastFocusDistance = Math.round(($scope.drivingData.lastFocusDistance + distanceDelta) * 100) / 100;
+        $scope.drivingData.withFocus = Math.round(($scope.drivingData.withFocus + distanceDelta) * 100) / 100;
+      }
+      
+      // we round it up to two decimal places, as js sucks in handling addition of floats.
+      $scope.drivingData.totalDistance = Math.round(($scope.drivingData.totalDistance + distanceDelta) * 100) / 100;
+    }, 1000);
+  };
+
+  // public
+  function stop() {
+    clearInterval(runningInterval);
+
+    $scope.isDriveMode = false;
+
+    hzTripRewards.addReward(
+      $scope.drivingData.totalDistance,
+      $scope.drivingData.withFocus,
+      $scope.drivingData.withoutFocus
+    );
+
+    initDrivingData();
+
+    $state.go('results');
+  }
+})
+
+.controller('ResultCtrl', function($scope, $state, hzTripRewards) {
+  $scope.reward = hzTripRewards.getLast();
+  $scope.startOver = startOver;
+  
+  if (!$scope.reward) {
+    startOver();
+  }
+
+  function startOver() {
+    $state.go('driving');
+  }
 });
